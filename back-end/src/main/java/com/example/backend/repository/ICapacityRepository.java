01a30373e88b2ef31ed75afb6640e472368eeff9
NguyenTrungHieu205 <117070722+NguyenTrungHieu205@users.noreply.github.com>
2023-08-21 23:49:24 +0700

    add

--- back-end/src/main/java/com/example/backend/repository/ICapacityRepository.java
+++ back-end/src/main/java/com/example/backend/repository/ICapacityRepository.java
@@ -3,5 +3,5 @@ package com.example.backend.repository;
 import com.example.backend.entity.Capacity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ICapacityRepository extends JpaRepository<Capacity , Integer> {
+public interface ICapacityRepository extends JpaRepository<Capacity, Integer> {
 }
