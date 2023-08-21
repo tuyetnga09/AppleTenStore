23fa109ea4410a12b1abacb6f8691a798c43ee19
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-08-21 23:19:37 +0700

    test

--- back-end/src/main/java/com/example/backend/controller/product_controller/controller/ColorController.java
+++ back-end/src/main/java/com/example/backend/controller/product_controller/controller/ColorController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/color/")
 public class ColorController {
