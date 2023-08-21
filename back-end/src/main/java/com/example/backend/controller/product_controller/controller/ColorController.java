210535c0f5dd4118481ad750e2cdc3947bb589e0
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-08-21 23:32:17 +0700

    test

--- back-end/src/main/java/com/example/backend/controller/product_controller/controller/ColorController.java
+++ back-end/src/main/java/com/example/backend/controller/product_controller/controller/ColorController.java
@@ -1,9 +1,9 @@
 package com.example.backend.controller.product_controller.controller;
 
-import com.example.backend.controller.product_controller.service.impl.CapacityServiceImpl;
 import com.example.backend.controller.product_controller.service.impl.CategoryServiceImpl;
-import com.example.backend.entity.Capacity;
+import com.example.backend.controller.product_controller.service.impl.ColorServiceImpl;
 import com.example.backend.entity.Category;
+import com.example.backend.entity.Color;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -19,33 +19,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/capacity/")
-public class CapacityController {
+@RequestMapping("/color/")
+public class ColorController {
     @Autowired
-    private CapacityServiceImpl capacityService;
+    private ColorServiceImpl colorService;
 
     @GetMapping("display")
-    public Page<Capacity> viewAll(@RequestParam(value = "page",defaultValue = "0") Integer page) {
+    public Page<Color> viewAll(@RequestParam(value = "page",defaultValue = "0") Integer page) {
         Pageable pageable = PageRequest.of(page,2);
-        return capacityService.getAll(pageable);
+        return colorService.getAll(pageable);
     }
 
     @PostMapping("save")
-    public void save(@RequestBody Capacity capacity) {
-        capacityService.insert(capacity);
+    public void save(@RequestBody Color color) {
+        colorService.insert(color);
     }
 
     @PutMapping("update/{id}")
-    public void update(@RequestBody Capacity capacity,@PathVariable("id") Integer id) {
-        capacity.setId(id);
-        capacityService.update(capacity, id);
+    public void update(@RequestBody Color color,@PathVariable("id") Integer id) {
+        color.setId(id);
+        colorService.update(color, id);
     }
 
-    //ham delete
     @DeleteMapping("delete")
     public void delete(@RequestParam("id")Integer id) {
-        capacityService.delete(id);
+        colorService.delete(id);
     }
-
-
 }
