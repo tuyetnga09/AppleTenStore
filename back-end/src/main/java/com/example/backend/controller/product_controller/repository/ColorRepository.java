19ba05420e47bda8a88280ae55fa1e667e6dbe1a
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-08-31 09:05:34 +0700

    update

--- back-end/src/main/java/com/example/backend/controller/product_controller/repository/ColorRepository.java
+++ back-end/src/main/java/com/example/backend/controller/product_controller/repository/ColorRepository.java
@@ -1,13 +1,15 @@
 package com.example.backend.controller.product_controller.repository;
 
-import com.example.backend.entity.Category;
 import com.example.backend.entity.Color;
 import com.example.backend.repository.IColorRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ColorRepository extends IColorRepository {
     Page<Color> findAll(Pageable  pageable);
+    @Query(value = "SELECT Id, Code, Name, date_create, date_update, person_create, person_update, Status  FROM Category WHERE Status = 1", nativeQuery = true)
+    Page<Color> deleteCategory(Pageable pageable);
 }
--- font-end/src/App.js
+++ font-end/src/App.js
@@ -4,18 +4,11 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import RamDisplay from "./components/product_component/Ram/DisplayRam";
 import RamCreateOrUpdate from "./components/product_component/Ram/DisplayRamCreate";
 import ReturnDeleteRam from "./components/product_component/Ram/DisplayReturnDelete";
-import ImportRam from "./components/product_component/Ram/DisplayImportRam";
 import ScreenDisplay from "./components/product_component/Screen/DisplayScreen";
 import ScreenCreateOrUpdate from "./components/product_component/Screen/DisplayScreenCreateOrUpdate";
 import ScreenDeleteScreen from "./components/product_component/Screen/DisplayReturnDeleteS";
-import DisplaySize from "./components/product_component/Size/Display";
-import FormAddOrUpdateSize from "./components/product_component/Size/FormAddOrUpdate";
-import ReturnDeleteSize from "./components/product_component/Size/ReturnDeleteSize";
-import ImportSize from "./components/product_component/Size/ImportSize";
-import DisplayBattery from "./components/product_component/Battery/Display";
-import FormAddOrUpdateBattery from "./components/product_component/Battery/FormAddOrUpdate";
-import ReturnDeleteBattery from "./components/product_component/Battery/ReturnDeleteBattery";
-import ImportBattery from "./components/product_component/Battery/ImportBattery";
+import Display from "./components/product_component/Size/Display";
+import FormAddOrUpdate from "./components/product_component/Size/FormAddOrUpdate";
 
 function App() {
   return (
@@ -27,35 +20,16 @@ function App() {
         {/* <div className="container"> */}
         <Switch>
           <Route path="/ram/display" exact component={RamDisplay} />
-          <Route path="/ram/im" exact component={ImportRam} />
           <Route path="/ram/displayDelete" exact component={ReturnDeleteRam} />
           <Route path="/ram/:id" exact component={RamCreateOrUpdate} />
 
-          <Route path="/size/getAll" exact component={DisplaySize} />
-          <Route path="/size/im" exact component={ImportSize} />
-          <Route
-            path="/size/displayDelete"
-            exact
-            component={ReturnDeleteSize}
-          />
-          <Route path="/size/:id" exact component={FormAddOrUpdateSize} />
-
-          <Route path="/battery/getAll" exact component={DisplayBattery} />
-          <Route path="/battery/im" exact component={ImportBattery} />
-          <Route
-            path="/battery/displayDelete"
-            exact
-            component={ReturnDeleteBattery}
-          />
-          <Route path="/battery/:id" exact component={FormAddOrUpdateBattery} />
+          <Route path="/size/getAll" exact component={Display} />
+          <Route path="/size/:id" exact component={FormAddOrUpdate} />
 
           <Route path="/screen/display" exact component={ScreenDisplay} />
-          <Route
-            path="/screen/displayDelete"
-            exact
-            component={ScreenDeleteScreen}
-          />
+          <Route path="/screen/displayDelete" exact component={ScreenDeleteScreen} />
           <Route path="/screen/:id" exact component={ScreenCreateOrUpdate} />
+
         </Switch>
         {/* </div> */}
       </Router>
