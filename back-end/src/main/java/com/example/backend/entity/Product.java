6af8a50f2eae12835d82c2f430aab428d4ea9699
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-09-05 22:25:48 +0700

    create product details

---
+++ back-end/src/main/java/com/example/backend/entity/Product.java
@@ -0,0 +1,65 @@
+package com.example.backend.entity;
+
+import com.example.backend.entity.dto.DuplicateAttribute;
+import com.example.backend.entity.dto.Identify;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product  extends DuplicateAttribute implements Identify {
+    private String code;
+    private String name;
+    private String description;
+    private Integer quantity;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_capacity")
+    private Capacity idcapacity;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_color")
+    private Color idcolor;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_manufacture")
+    private Manufacture idmanufacture;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_category")
+    private Category idcategory;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_battery")
+    private Battery idbattery;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_chip")
+    private Chip idchip;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_ram")
+    private Ram idRam;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_screen")
+    private Screen idscreen;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_size")
+    private Size idsize;
+
+
+
+}
