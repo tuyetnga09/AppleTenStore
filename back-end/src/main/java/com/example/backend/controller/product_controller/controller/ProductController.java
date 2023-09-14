8f34a8084265d240b7fc841fa5c6a76f0773cf82
Hoang Hieu <hieuhv203@gmail.com>
2023-09-14 14:52:40 +0700

    Upload image kimote

--- back-end/src/main/java/com/example/backend/controller/product_controller/controller/ProductController.java
+++ back-end/src/main/java/com/example/backend/controller/product_controller/controller/ProductController.java
@@ -1,7 +1,6 @@
 package com.example.backend.controller.product_controller.controller;
 
 import com.example.backend.controller.product_controller.repository.ProductRepository;
-import com.example.backend.controller.product_controller.service.impl.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/product/")
 public class ProductController {
+
     @Autowired
     private ProductRepository productRepository;
-    @Autowired
-    private ProductServiceImpl productService;
-
-
-
 
 }
