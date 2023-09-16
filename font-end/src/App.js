6123b7b977b0103fe08c3f59696af29803934769
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-09-16 23:33:26 +0700

    update

--- font-end/src/App.js
+++ font-end/src/App.js
@@ -42,15 +42,21 @@ import AddColor from "./components/product_component/Color/DislayColorCreate";
 import ColorDisplayReturn from "./components/product_component/Color/DisplayReturnDelete";
 import ScanColor from "./components/product_component/Color/DisplayScanQRColor";
 import ImageFormAddOrUpdate from "./components/product_component/image/FormAddOrUpdate";
+import Display from "./components/product_component/image/Display"
 
-import TrangChu from "./components/Page_Comeponet/TrangChu";
+import Home from "./components/Page_Comeponet/TrangChu";
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
+<<<<<<<<< Temporary merge branch 1
+          
+          <Route path="/" exact component={Home} />
+=========
           <Route path="/" exact component={TrangChu} />
+>>>>>>>>> Temporary merge branch 2
 
           <Route path="/ram/display" exact component={RamDisplay} />
           <Route path="/ram/im" exact component={ImportRam} />
@@ -131,6 +137,12 @@ function App() {
           <Route path="/chip/:id" exact component={AddChip} />
           <Route path="/chip/scan" exact component={ScanChip} />
 
+<<<<<<<<< Temporary merge branch 1
+          <Route path="/image/display" exact component={Display} />
+          <Route path="/image/:id" exact component={ImageFormAddOrUpdate} />
+
+         
+=========
           <Route path="/color/getAll" exact component={DisplayColor} />
           <Route
             path="/color/displayDelete"
@@ -140,6 +152,7 @@ function App() {
           <Route path="/color/im" exact component={ImportColor} />
           <Route path="/color/:id" exact component={AddColor} />
           <Route path="/color/scan" exact component={ScanColor} />
+>>>>>>>>> Temporary merge branch 2
 
           <Route path="/image/:id" exact component={ImageFormAddOrUpdate} />
         </Switch>
