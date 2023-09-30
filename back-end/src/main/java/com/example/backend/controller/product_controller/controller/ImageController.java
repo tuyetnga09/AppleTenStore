6541021093ebd938ac8de84d0c7ce6ef8cc7875a
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-09-30 14:06:42 +0700

    [update data]

--- back-end/src/main/java/com/example/backend/controller/product_controller/controller/ImageController.java
+++ back-end/src/main/java/com/example/backend/controller/product_controller/controller/ImageController.java
@@ -18,6 +18,7 @@ public class ImageController {
 
     @Autowired
     private ImageService service;
+    //hihi
 
     @GetMapping(value = "/display")
     public Page<Image> viewAll(@RequestParam(value = "page", defaultValue = "0") Integer page){
