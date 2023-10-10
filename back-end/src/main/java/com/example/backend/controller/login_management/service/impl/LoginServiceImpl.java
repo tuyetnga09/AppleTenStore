9419d26196ea9cb8ebb2a71be4f35e2d59057667
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-10-10 15:35:32 +0700

    update resetPassword

--- back-end/src/main/java/com/example/backend/controller/login_management/service/impl/LoginServiceImpl.java
+++ back-end/src/main/java/com/example/backend/controller/login_management/service/impl/LoginServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.backend.controller.login_management.service.impl;
 
 import com.example.backend.controller.login_management.model.request.LoginRequest;
+import com.example.backend.controller.login_management.model.request.ResetRequest;
 import com.example.backend.controller.login_management.model.response.LoginResponse;
 import com.example.backend.controller.login_management.service.LoginService;
+import com.example.backend.controller.order_management.model.EmailService;
 import com.example.backend.entity.Account;
 import com.example.backend.repository.AccountRepository;
 import com.example.backend.untils.Message;
@@ -11,12 +13,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class LoginServiceImpl  implements LoginService {
 
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private EmailService emailService;
 
     private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -29,6 +35,22 @@ public class LoginServiceImpl  implements LoginService {
         if (!passwordEncoder.matches(request.getPassword(), account.getPassword())) {
             throw new RestAPIRunTime((Message.PASSWORD_NOT_EXISTS));
         }
+        LoginResponse response = new LoginResponse(account);
+        return response;
+    }
+
+    @Override
+    public LoginResponse resetPassword(ResetRequest resetPassword) {
+        Account account = accountRepository.resetPassword(resetPassword.getEmailForgot(), resetPassword.getPhoneNumber());
+        if (account == null) {
+            throw new RestAPIRunTime(Message.NOT_EXISTS);
+        }
+        Random random = new Random();
+        int randomNumber = 100000 + random.nextInt(900000);
+        account.setPassword(passwordEncoder.encode(String.valueOf(randomNumber)));
+        accountRepository.save(account);
+        String subject = "Mật khẩu mới của bạn ";
+//        emailService.sendEmailPasword(account.getEmail(),subject,String.valueOf(randomNumber));
         LoginResponse response = new LoginResponse(account);
         return response;
     }
--- back-end/src/main/java/com/example/backend/repository/AccountRepository.java
+++ back-end/src/main/java/com/example/backend/repository/AccountRepository.java
@@ -21,4 +21,7 @@ public interface AccountRepository extends JpaRepository<Account, Integer> {
     @Query(value = "SELECT SUM(CASE WHEN MONTH(date_create) = MONTH(CURRENT_DATE) AND YEAR(date_create) = YEAR(CURRENT_DATE) THEN 1 ELSE 0 END) from account", nativeQuery = true)
     Integer numberOfCustomersThisMonth();
 
+    @Query("SELECT ac FROM Account ac WHERE ac.email =:email AND ac.user.phoneNumber =:phoneNumber")
+    Account resetPassword(@Param("email") String email , @Param("phoneNumber") String phoneNumber);
+
 }
