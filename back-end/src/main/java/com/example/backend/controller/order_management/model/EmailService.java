c36fb926ef03360c12f259cb20909ce0ee535a32
MinhTC23 <trankongmin@gmail.com>
2023-10-10 10:43:31 +0700

    update send mail

--- back-end/src/main/java/com/example/backend/controller/order_management/model/EmailService.java
+++ back-end/src/main/java/com/example/backend/controller/order_management/model/EmailService.java
@@ -1,22 +1,29 @@
 package com.example.backend.controller.order_management.model;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
+    public void sendEmailWithHtml(String to, String subject, String htmlContent) {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-    public void sendEmail(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        javaMailSender.send(message);
+            helper.setTo(to);
+            helper.setSubject(subject);
+
+            helper.setText(htmlContent, true);
+
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
-
-
 }
