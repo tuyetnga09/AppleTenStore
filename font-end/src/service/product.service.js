081f66d0a31e2b5d3ae1ad3618c424cf982f9d97
MinhTC23 <trankongmin@gmail.com>
2023-10-11 23:29:06 +0700

    update

--- font-end/src/service/product.service.js
+++ font-end/src/service/product.service.js
@@ -12,31 +12,31 @@ export const readAll = (page) => {
 
 // hiển thị comboobox
 export const readAllColor = () => {
-  return httpClient.get(`/color/get-all-color`);
+  return httpClient.get(`/admin/color/get-all-color`);
 };
 export const readAllChip = () => {
-  return httpClient.get(`/chip/get-all-chip`);
+  return httpClient.get(`/admin/chip/get-all-chip`);
 };
 export const readAllBattery = () => {
-  return httpClient.get(`/battery/get-all-battery`);
+  return httpClient.get(`/admin/battery/get-all-battery`);
 };
 export const readAllCapacity = () => {
-  return httpClient.get(`/capacity/get-all-capacity`);
+  return httpClient.get(`/admin/capacity/get-all-capacity`);
 };
 export const readAllCategory = () => {
-  return httpClient.get(`/category/get-all-category`);
+  return httpClient.get(`/admin/category/get-all-category`);
 };
 export const readAllManufacture = () => {
-  return httpClient.get(`/manufacture/get-all-manufacture`);
+  return httpClient.get(`/admin/manufacture/get-all-manufacture`);
 };
 export const readAllRam = () => {
-  return httpClient.get(`/ram/get-all-ram`);
+  return httpClient.get(`/admin/ram/get-all-ram`);
 };
 export const readAllScreen = () => {
-  return httpClient.get(`/screen/get-all-screen`);
+  return httpClient.get(`/admin/screen/get-all-screen`);
 };
 export const readAllSize = () => {
-  return httpClient.get(`/size/get-all-size`);
+  return httpClient.get(`/admin/size/get-all-size`);
 };
 export const returnDeleteAll = (page) => {
   return httpClient.get(`/admin/product/displayDelete?${page}`);
