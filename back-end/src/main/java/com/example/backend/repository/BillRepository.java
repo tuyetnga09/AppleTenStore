391b8405f4a77d46fb1d1e4ca96480c4917f7ba5
phongStudentDEV <116378201+phongStudentDEV@users.noreply.github.com>
2023-11-26 22:40:33 +0700

    update dasbroach

--- back-end/src/main/java/com/example/backend/repository/BillRepository.java
+++ back-end/src/main/java/com/example/backend/repository/BillRepository.java
@@ -34,11 +34,7 @@ List<Bill> searchWithDate(String key, String status, LocalDate dateStart, LocalD
 
 
     //    @Query(value = "select * from bill where DATE(date_create) = CURDATE()", nativeQuery = true)
-    @Query(value = "SELECT * FROM bill WHERE CASE " +
-            " WHEN date_update IS NULL AND DATE(date_create) = CURDATE() THEN 1\n" +
-            " WHEN date_update IS NOT NULL AND DATE(date_update) = CURDATE() THEN 1\n" +
-            " ELSE 0\n" +
-            " END = 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM bill WHERE DATE(date_create) = CURDATE()", nativeQuery = true)
     List<Bill> listOfBillsForTheDay();
 
     //tổng tiền chưa trừ  đơn huỷ
@@ -78,49 +74,60 @@ List<Bill> searchWithDate(String key, String status, LocalDate dateStart, LocalD
 
 
     // lấy ra list imei máy bán ra trong 30 ngày gần đây
-    @Query(value = "select codeImei from Imei_DaBan join bill_detail on bill_detail.id = Imei_DaBan.IdBillDetail " +
-            "    where (" +
-            "      (bill_detail.date_update IS NULL AND DATEDIFF(CURDATE(), bill_detail.date_create) < 30)" +
-            "    OR (bill_detail.date_update IS NOT NULL AND DATEDIFF(CURDATE(), bill_detail.date_update) < 30) " +
-            "        )", nativeQuery = true)
+    @Query(value = "select code_imei from imei_da_ban join bill_detail on bill_detail.id = imei_da_ban.id_bill_detail \n" +
+            "                where (\n" +
+            "                  (bill_detail.date_update IS NULL AND DATEDIFF(CURDATE(), bill_detail.date_create) < 30)\n" +
+            "               OR (bill_detail.date_update IS NOT NULL AND DATEDIFF(CURDATE(), bill_detail.date_update) < 30) \n" +
+            "                   )", nativeQuery = true)
     List<String> listImeiDaBanTrong30NgayGanDay();
 
 
     // lấy ra list imei máy bán ra trong 30 ngày gần đây
 //    @Transactional
 //    @Modifying
-    @Query(value = "SELECT \n" +
-            "          MONTH(date_create) AS  month , \n" +
-            "              SUM(total_money) AS totalMoney, \n" +
-            "              count(code) as quantity\n" +
-            "              FROM bill \n" +
-            "                WHERE YEAR(date_create) = 2023 and status_bill = 'DA_THANH_TOAN' \n" +
-            "              GROUP BY month\n" +
-            "               ORDER BY month", nativeQuery = true)
+    @Query(value = "SELECT\n" +
+            "  MONTH(date_create) AS month,\n" +
+            "  SUM(total_money) AS totalMoney,\n" +
+            "  COUNT(code) AS quantity\n" +
+            "FROM bill\n" +
+            "WHERE YEAR(date_create) = 2023 AND status_bill = 'DA_THANH_TOAN'\n" +
+            "GROUP BY month\n" +
+            "UNION\n" +
+            "SELECT\n" +
+            "  Months.MonthNumber AS month,\n" +
+            "  0 AS totalMoney,\n" +
+            "  0 AS quantity\n" +
+            "FROM (SELECT 1 AS MonthNumber UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6\n" +
+            "      UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 10 UNION SELECT 11 UNION SELECT 12) Months\n" +
+            "LEFT JOIN bill ON Months.MonthNumber = MONTH(bill.date_create) AND YEAR(bill.date_create) = 2023 AND bill.status_bill = 'DA_THANH_TOAN'\n" +
+            "WHERE bill.code IS NULL\n" +
+            "ORDER BY month", nativeQuery = true)
     List<AnnualRevenueIon> annualRevenueYear();
 
     //Customers
 
     // lấy râ số khách hàng đã đặt hàng hôm nay,
     @Query(value = "select account.id from bill join account on bill.id_account = account.id \n" +
-            " where date(bill.date_create) = CURDATE() and bill.status_bill='CHO_XAC_NHAN'", nativeQuery = true)
+            " where date(bill.date_create) = CURDATE() and bill.status_bill='CHO_XAC_NHAN' group by  account.id", nativeQuery = true)
     List<Integer> countCustomersOrderToday();
 
     //số khác hàng đã huỷ đơn hôm nay
     @Query(value = "select account.id from bill join account on bill.id_account = account.id \n" +
-            " where date(bill.date_create) = CURDATE() and bill.status_bill='DA_HUY'", nativeQuery = true)
+            " where date(bill.date_create) = CURDATE() and bill.status_bill='DA_HUY' group by  account.id", nativeQuery = true)
     List<Integer> countCustomersCanceledToday();
+
     //số khách hàng đã thanh toán hôm nay
 
     @Query(value = "select account.id from bill join account on bill.id_account = account.id \n" +
-            " where date(bill.date_create) = CURDATE() and bill.status_bill='DA_HUY'", nativeQuery = true)
+            " where date(bill.date_create) = CURDATE() and bill.status_bill='DA_THANH_TOAN' group by  account.id", nativeQuery = true)
     List<Integer> countCustomersPaidToday();
 
     // số khách hàng trả đơn trong hôm nay
 
     @Query(value = "select account.id from bill join account on bill.id_account = account.id \n" +
-            " where date(bill.date_create) = CURDATE() and bill.status_bill='TRA_HANG'", nativeQuery = true)
+            " where date(bill.date_create) = CURDATE() and bill.status_bill='TRA_HANG' group by  account.id", nativeQuery = true)
     List<Integer> countCustomersReturnedToday();
+
     @Query(value = "select * from bill where id_account = ?1 and type = 'ONLINE'", nativeQuery = true)
     List<Bill> listBillByIdAccount(Integer id);
 
@@ -163,4 +170,56 @@ List<Bill> searchWithDate(String key, String status, LocalDate dateStart, LocalD
 
     @Query(value = "SELECT * FROM bill WHERE code like ?1", nativeQuery = true)
     List<Bill> getThongTinThanhToan(String codeBill);
+
+    @Query(value = "SELECT * FROM bill\n" +
+            "    WHERE DATE(date_create) = CURDATE() AND status_bill = 'CHO_VAN_CHUYEN'\n" +
+            "    ORDER BY   date_create DESC, Id DESC", nativeQuery = true)
+    List<Bill> listHoaDonChoVanChuyenTrongNgay();
+
+    //lấy ra tổng số bill của các bill có tài khoản trong ngày hôm nay
+    @Query(value = "select * from bill join account on bill.id_account = account.id\n" +
+            "    where date(bill.date_create) = CURDATE()", nativeQuery = true)
+    List<Bill> getAllBillOfAccountToDay();
+
+    // lấy ra list bill trong khoảng ngày a- ngày b
+    @Query(value = "select * from bill where date_create BETWEEN ?1 and ?2", nativeQuery = true)
+    List<Bill> getBillSeach(LocalDate dateBefore, LocalDate dateAfter);
+
+    // lấy râ số khách hàng đã đặt hàng khoang ngay
+    @Query(value = "select account.id from bill join account on bill.id_account = account.id \n" +
+            " where bill.date_create  BETWEEN ?1 and ?2  and bill.status_bill='CHO_XAC_NHAN' group by  account.id", nativeQuery = true)
+    List<Integer> getListKhachHangDatHangHomNay(LocalDate dateBefore, LocalDate dateAfter);
+
+    // lấy râ số khách hàng đã thanh toan trong khoang ngay
+    @Query(value = "select account.id from bill join account on bill.id_account = account.id \n" +
+            " where bill.date_create  BETWEEN ?1 and ?2 and bill.status_bill='DA_THANH_TOAN' group by  account.id", nativeQuery = true)
+    List<Integer> getListKhachHangDaThanhToanTrongKhoangNgay(LocalDate dateBefore, LocalDate dateAfter);
+
+    // số khách hàng huy đơn trong khoang ngay
+    @Query(value = "select account.id from bill join account on bill.id_account = account.id " +
+            "   where bill.date_create  BETWEEN ?1 and ?2 and bill.status_bill='DA_HUY' group by  account.id", nativeQuery = true)
+    List<Integer> getListKhachHangDaHuyDonTrongKhoangNgay(LocalDate dateBefore, LocalDate dateAfter);
+
+    // list năm
+    @Query(value = "select year(date_create) from bill where status_bill='DA_THANH_TOAN' group by year(date_create)", nativeQuery = true)
+    List<Integer> getListYearOfBill();
+
+    @Query(value = "SELECT\n" +
+            "  MONTH(date_create) AS month,\n" +
+            "  SUM(total_money) AS totalMoney,\n" +
+            "  COUNT(code) AS quantity\n" +
+            "FROM bill\n" +
+            "WHERE YEAR(date_create) =?1 AND status_bill = 'DA_THANH_TOAN'\n" +
+            "GROUP BY month\n" +
+            "UNION\n" +
+            "SELECT\n" +
+            "  Months.MonthNumber AS month,\n" +
+            "  0 AS totalMoney,\n" +
+            "  0 AS quantity\n" +
+            "FROM (SELECT 1 AS MonthNumber UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6\n" +
+            "      UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 10 UNION SELECT 11 UNION SELECT 12) Months\n" +
+            "LEFT JOIN bill ON Months.MonthNumber = MONTH(bill.date_create) AND YEAR(bill.date_create) = 2023 AND bill.status_bill = 'DA_THANH_TOAN'\n" +
+            "WHERE bill.code IS NULL\n" +
+            "ORDER BY month", nativeQuery = true)
+    List<AnnualRevenueIon> seachDoanhSoTheoNam(Integer year);
 }
--- font-end/src/components/Page_Comeponet/dashboard/dailyOrders/index.js
+++ font-end/src/components/Page_Comeponet/dashboard/dailyOrders/index.js
@@ -1,108 +1,33 @@
-// import { useMemo } from "react";
-// import { useApiUrl, useCustom, useTranslate } from "@refinedev/core";
-// import { Typography } from "antd";
-// import { Column } from "@ant-design/charts";
-// import { ColumnConfig } from "@ant-design/plots/lib/components/column";
+import { useEffect, useState } from "react";
+import { Alert, Typography } from "antd";
 
-// import { IncreaseIcon } from "../../icon/increase";
-// import { DecreaseIcon } from "../../icon/decrease";
-
-// // import { ISalesChart } from "../../../interfaces";
-// import { DailyOrderWrapper, TitleAreNumber, TitleArea } from "./styled";
-
-// const DailyOrders = () => {
-//   const t = useTranslate();
-//   const API_URL = useApiUrl();
-
-//   const url = `${API_URL}/dailyOrders`;
-//   const { data, isLoading } =
-//     useCustom <
-//     {
-//       data: null,
-//       total: 0,
-//       trend: 0,
-//     } >
-//     { url, method: "get" };
-
-//   const { Text, Title } = Typography;
-
-//   const config = useMemo(() => {
-//     const config = (ColumnConfig = {
-//       data: data?.data.data || [],
-//       loading: isLoading,
-//       padding: 0,
-//       xField: "date",
-//       yField: "value",
-//       maxColumnWidth: 16,
-//       columnStyle: {
-//         radius: [4, 4, 0, 0],
-//       },
-//       color: "rgba(255, 255, 255, 0.5)",
-//       tooltip: {
-//         customContent: (title, data) => {
-//           return `<div style="padding: 8px 4px; font-size:16px; font-weight:600">${data[0]?.value}</div>`;
-//         },
-//       },
-
-//       xAxis: {
-//         label: null,
-//         line: null,
-//         tickLine: null,
-//       },
-//       yAxis: {
-//         label: null,
-//         grid: null,
-//         tickLine: null,
-//       },
-//     });
-
-//     return config;
-//   }, [data]);
-
-//   return (
-//     <DailyOrderWrapper>
-//       <TitleArea>
-//         <Title level={3}>Daily Orders</Title>
-//         <TitleAreNumber>
-//           <Text strong>{data?.data.total ?? 0} </Text>
-
-//           {/* {(data?.data?.trend ?? 0) > 0 ? <IncreaseIcon /> : <DecreaseIcon />} */}
-//           <IncreaseIcon />
-//           {/* <DecreaseIcon /> */}
-//         </TitleAreNumber>
-//       </TitleArea>
-//       <Column
-//         style={{ padding: 0, height: 135 }}
-//         appendPadding={10}
-//         {...config}
-//       />
-//     </DailyOrderWrapper>
-//   );
-// };
-
-// export default DailyOrders;
-
-import {useEffect, useState} from "react";
-import {Typography} from "antd";
-
-import {IncreaseIcon} from "../../icon/increase";
+import { IncreaseIcon } from "../../icon/increase";
 import {
-    sumAllBill,
-    sumBillAlreadyPaid,
-    sumBillAreDelivering,
-    sumBillCancelOrder,
-    sumBillConfirmed,
-    sumBillNoReturn,
-    sumBillReturns,
-    sumBillUnconfimred,
+  sumAllBill,
+  sumBillAlreadyPaid,
+  sumBillAreDelivering,
+  sumBillCancelOrder,
+  sumBillConfirmed,
+  sumBillNoReturn,
+  sumBillReturns,
+  sumBillUnconfimred,
 } from "../../../../service/dashboard/admin_bill.service";
-
+import React, { useRef } from "react";
+import isEqual from "lodash/isEqual";
 // import { ISalesChart } from "../../../interfaces";
-import {DailyOrderWrapper, TitleArea, TitleAreNumber} from "./styled";
-import {CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis,} from "recharts";
+import { DailyOrderWrapper, TitleArea, TitleAreNumber } from "./styled";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
-const DailyOrders = () => {
-  const [data, setData] = useState([]);
+const DailyOrders = (props) => {
+  const [dataDailyOrder, setDataDailyOrder] = useState([]);
   const [dataSumAllBill, setDataSumAllBill] = useState();
   const [dataSumUnconfimred, setDataSumUnconfimred] = useState();
   const [dataSumConfimred, setDataSumConfimred] = useState();
@@ -113,100 +38,27 @@ const DailyOrders = () => {
   const [dataSumCancelOrder, setDataSumCancelOrder] = useState();
   const [dataOrder, setDataOrder] = useState([]);
   // Simulate data from the backend
-
+  const prevData = useRef();
+  const [billSeachKhoangNgay, setLocalStorageData] = useState(
+    localStorage.getItem("billSeachKhoangNgay")
+  );
   useEffect(() => {
-    let unconfirmedData = null;
-    let confirmedData = null;
-    let alreadyPaidData = null;
-    let areDeliveringData = null;
-    let noReturnData = null;
-    let returnsData = null;
-    let cancelOrderData = null;
-    //1
-    sumAllBill()
-      .then((response) => {
-        console.log(response.data);
-        setDataSumAllBill(response.data);
-      })
-      .catch((error) => {
-        console.log(`${error}`);
-      });
-
-    //2
-    sumBillUnconfimred()
-      .then((response) => {
-        console.log(response.data);
-        setDataSumUnconfimred(response.data);
-        // setDataOrder([...dataOrder, response.data]);
-        unconfirmedData = response.data;
-      })
-      .catch((error) => {
-        console.log(`${error}`);
-      });
-    //3
-    sumBillConfirmed()
-      .then((response) => {
-        console.log(response.data);
-        setDataSumConfimred(response.data);
-        confirmedData = response.data;
-        // setDataOrder(response.data);
-      })
-      .catch((error) => {
-        console.log(`${error}`);
-      });
-
-    //4
-    sumBillAreDelivering()
-      .then((response) => {
-        console.log(response.data);
-        setDataSumAreDelivering(response.data);
-        areDeliveringData = response.data;
-        // setDataOrder(response.data);
-      })
-      .catch((error) => {
-        console.log(`${error}`);
-      });
-    //5
-    sumBillAlreadyPaid()
-      .then((response) => {
-        console.log(response.data);
-        setDataSumAlreadyPaid(response.data);
-        alreadyPaidData = response.data;
-        // setDataOrder(response.data);
-      })
-      .catch((error) => {
-        console.log(`${error}`);
-      });
-    //6
-    sumBillNoReturn()
-      .then((response) => {
-        console.log(response.data);
-        setDataSumNoReturn(response.data);
-        noReturnData = response.data;
-        // setDataOrder(response.data);
-      })
-      .catch((error) => {
-        console.log(`${error}`);
-      });
-    //7
-    sumBillReturns()
-      .then((response) => {
-        console.log(response.data);
-        setDataSumReturns(response.data);
-        returnsData = response.data;
-        // setDataOrder(response.data);
-      })
-      .catch((error) => {
-        console.log(`${error}`);
-      });
-    //8
-    sumBillCancelOrder()
-      .then((response) => {
-        console.log(response.data);
-        setDataSumCancelOrder(response.data);
-        cancelOrderData = response.data;
-        // setDataOrder(response.data);
-        // Gọi fetchDataFromBackend sau khi nhận được cả hai dữ liệu
+    let unconfirmedData = 0;
+    let confirmedData = 0;
+    let alreadyPaidData = 0;
+    let areDeliveringData = 0;
+    let noReturnData = 0;
+    let returnsData = 0;
+    let cancelOrderData = 0;
+    if (billSeachKhoangNgay !== null) {
+      if (billSeachKhoangNgay.check === true) {
+        unconfirmedData = billSeachKhoangNgay.choXacNhan;
+        confirmedData = billSeachKhoangNgay.choVanChuyen;
+        areDeliveringData = billSeachKhoangNgay.dangVanChuyen;
+        alreadyPaidData = billSeachKhoangNgay.daThanhToan;
+        noReturnData = 0;
+        returnsData = billSeachKhoangNgay.hoanTra;
+        cancelOrderData = billSeachKhoangNgay.donHuy;
         fetchDataFromBackend(
           unconfirmedData,
           confirmedData,
@@ -216,26 +68,177 @@ const DailyOrders = () => {
           returnsData,
           cancelOrderData
         );
-      })
-      .catch((error) => {
-        console.log(`${error}`);
-      });
+
+        setDataSumAllBill(billSeachKhoangNgay.sumBill);
+        setDataSumUnconfimred(billSeachKhoangNgay.choXacNhan);
+        setDataSumConfimred(billSeachKhoangNgay.choVanChuyen);
+        setDataSumAreDelivering(billSeachKhoangNgay.dangVanChuyen);
+        setDataSumAlreadyPaid(billSeachKhoangNgay.daThanhToan);
+        setDataSumNoReturn(0);
+        setDataSumReturns(billSeachKhoangNgay.hoanTra);
+        setDataSumCancelOrder(billSeachKhoangNgay.donHuy);
+      }
+    } else {
+      //1
+      sumAllBill()
+        .then((response) => {
+          console.log(response.data);
+          setDataSumAllBill(response.data);
+        })
+        .catch((error) => {
+          console.log(`${error}`);
+        });
+      //2
+      sumBillUnconfimred()
+        .then((response) => {
+          setDataSumUnconfimred(response.data);
+          // setDataOrder([...dataOrder, response.data]);
+          unconfirmedData = response.data;
+          fetchDataFromBackend(
+            unconfirmedData,
+            confirmedData,
+            areDeliveringData,
+            alreadyPaidData,
+            noReturnData,
+            returnsData,
+            cancelOrderData
+          );
+        })
+        .catch((error) => {
+          console.log(`${error}`);
+        });
+      //3
+      sumBillConfirmed()
+        .then((response) => {
+          setDataSumConfimred(response.data);
+          confirmedData = response.data;
+          // setDataOrder(response.data);
+          fetchDataFromBackend(
+            unconfirmedData,
+            confirmedData,
+            areDeliveringData,
+            alreadyPaidData,
+            noReturnData,
+            returnsData,
+            cancelOrderData
+          );
+        })
+        .catch((error) => {
+          console.log(`${error}`);
+        });
+
+      //4
+      sumBillAreDelivering()
+        .then((response) => {
+          setDataSumAreDelivering(response.data);
+          areDeliveringData = response.data;
+          // setDataOrder(response.data);
+          fetchDataFromBackend(
+            unconfirmedData,
+            confirmedData,
+            areDeliveringData,
+            alreadyPaidData,
+            noReturnData,
+            returnsData,
+            cancelOrderData
+          );
+        })
+        .catch((error) => {
+          console.log(`${error}`);
+        });
+      //5
+      sumBillAlreadyPaid()
+        .then((response) => {
+          setDataSumAlreadyPaid(response.data);
+          alreadyPaidData = response.data;
+          // setDataOrder(response.data);
+          fetchDataFromBackend(
+            unconfirmedData,
+            confirmedData,
+            areDeliveringData,
+            alreadyPaidData,
+            noReturnData,
+            returnsData,
+            cancelOrderData
+          );
+        })
+        .catch((error) => {
+          console.log(`${error}`);
+        });
+      //6
+      sumBillNoReturn()
+        .then((response) => {
+          console.log(response.data);
+          setDataSumNoReturn(response.data);
+          noReturnData = response.data;
+          // setDataOrder(response.data);
+          fetchDataFromBackend(
+            unconfirmedData,
+            confirmedData,
+            areDeliveringData,
+            alreadyPaidData,
+            noReturnData,
+            returnsData,
+            cancelOrderData
+          );
+        })
+        .catch((error) => {
+          console.log(`${error}`);
+        });
+      //7
+      sumBillReturns()
+        .then((response) => {
+          setDataSumReturns(response.data);
+          returnsData = response.data;
+          // setDataOrder(response.data);
+          fetchDataFromBackend(
+            unconfirmedData,
+            confirmedData,
+            areDeliveringData,
+            alreadyPaidData,
+            noReturnData,
+            returnsData,
+            cancelOrderData
+          );
+        })
+        .catch((error) => {
+          console.log(`${error}`);
+        });
+      //8
+      sumBillCancelOrder()
+        .then((response) => {
+          setDataSumCancelOrder(response.data);
+          cancelOrderData = response.data;
+          // setDataOrder(response.data);
+          // Gọi fetchDataFromBackend sau khi nhận được cả hai dữ liệu
+          fetchDataFromBackend(
+            unconfirmedData,
+            confirmedData,
+            areDeliveringData,
+            alreadyPaidData,
+            noReturnData,
+            returnsData,
+            cancelOrderData
+          );
+        })
+        .catch((error) => {
+          console.log(`${error}`);
+        });
+    }
   }, []);
 
   const fetchDataFromBackend = (a, b, c, d, e, f, g) => {
-    console.log(a + " " + b + " " + c + " " + d + " " + e + " " + f + " " + g);
-    console.log(dataOrder + " -------oder 2 : ");
     // Replace this with actual API call to your backend
     const dataFromBackend = [
       { name: "1U", orders: a },
       { name: "2C", orders: b },
       { name: "3AD", orders: c },
       { name: "4AP", orders: d },
-      { name: "5NR", orders: e },
-      { name: "6R", orders: f },
-      { name: "7CO", orders: g },
+      // { name: "5NR", orders: e },
+      { name: "5R", orders: f },
+      { name: "6CO", orders: g },
     ];
-    setData(dataFromBackend);
+    setDataDailyOrder(dataFromBackend);
   };
 
   const { Text, Title } = Typography;
@@ -256,7 +259,7 @@ const DailyOrders = () => {
         <Text strong>-</Text>
       </div>
 
-      <LineChart width={330} height={150} data={data}>
+      <LineChart width={330} height={150} data={dataDailyOrder}>
         <XAxis dataKey="name" />
         <YAxis />
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
@@ -278,7 +281,7 @@ const DailyOrders = () => {
       {/* 2 */}
       <TitleArea>
         <Title level={5} style={{ color: "white" }}>
-          2C - Đơn hang đã xác nhận
+          2C - Đơn hàng chờ vận chuyển
         </Title>
         <TitleAreNumber>
           <Title level={5} style={{ color: "white" }}>
@@ -310,7 +313,7 @@ const DailyOrders = () => {
         </TitleAreNumber>
       </TitleArea>
       {/* 5 */}
-      <TitleArea>
+      {/* <TitleArea>
         <Title level={5} style={{ color: "white" }}>
           5NR - Đơn hàng không hoàn trả
         </Title>
@@ -319,11 +322,11 @@ const DailyOrders = () => {
             {dataSumNoReturn}
           </Title>
         </TitleAreNumber>
-      </TitleArea>
+      </TitleArea> */}
       {/* 6 */}
       <TitleArea>
         <Title level={5} style={{ color: "white" }}>
-          6R - Đơn hàng hoàn trả
+          5R - Đơn hàng hoàn trả
         </Title>
         <TitleAreNumber>
           <Title level={5} style={{ color: "white" }}>
@@ -334,7 +337,7 @@ const DailyOrders = () => {
       {/* 7 */}
       <TitleArea>
         <Title level={5} style={{ color: "white" }}>
-          7CO - Đơn hàng huỷ
+          6CO - Đơn hàng huỷ
         </Title>
         <TitleAreNumber>
           <Title level={5} style={{ color: "white" }}>
